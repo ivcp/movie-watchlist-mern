aff8463998993faa476d1373c1d533f1bc86860f
ivica <blizujemars@gmail.com>
2023-01-07 11:37:18 +0100

    refactor tmbd controller, add fetchMovies helper

--- server/controllers/tmdb.js
+++ server/controllers/tmdb.js
@@ -1,48 +1,35 @@
 const tmdbRouter = require('express').Router();
 const config = require('../utils/config');
+const fetchMovies = require('../utils/fetchMovies');
 
 tmdbRouter.get('/popular', async (req, res) => {
-  const response = await fetch(
-    `${config.POPULAR}api_key=${config.API_KEY}&language=en-US&page=1`
+  fetchMovies(
+    `${config.POPULAR}api_key=${config.API_KEY}&language=en-US&page=1`,
+    req,
+    res
   );
-  if (!response.ok) {
-    const error = await response.json();
-    return res.status(400).json(error);
-  }
-  const { results } = await response.json();
-  res.json(results);
 });
 
 tmdbRouter.get('/search', async (req, res) => {
   const { query } = req.body;
 
-  const response = await fetch(
-    `${config.SEARCH}api_key=${config.API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+  fetchMovies(
+    `${config.SEARCH}api_key=${config.API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`,
+    req,
+    res
   );
-  if (!response.ok) {
-    const error = await response.json();
-    return res.status(400).json(error);
-  }
-  const { results } = await response.json();
-  res.json(results);
 });
 
 tmdbRouter.get('/details', async (req, res) => {
   const { movieId } = req.body;
-  try {
-    const response = await fetch(
-      `${config.DETAILS}${movieId}?api_key=${config.API_KEY}&language=en-US`
-    );
-    if (!response.ok) {
-      const error = await response.json();
-      return res.status(400).json(error);
-    }
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error.message });
-  }
+  const details = true;
+
+  fetchMovies(
+    `${config.DETAILS}${movieId}?api_key=${config.API_KEY}&language=en-US`,
+    req,
+    res,
+    details
+  );
 });
 
 module.exports = tmdbRouter;
---
+++ server/utils/fetchMovies.js
@@ -0,0 +1,18 @@
+const fetchMovies = async (url, req, res, details) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = await response.json();
+    return res.status(400).json(error);
+  }
+  if (!details) {
+    const { results } = await response.json();
+    res.json(results);
+  }
+
+  if (details) {
+    const data = await response.json();
+    res.json(data);
+  }
+};
+
+module.exports = fetchMovies;
