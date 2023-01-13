066e96ef2a5f4b0611f213e7286d86cc7e529a0d
ivica <blizujemars@gmail.com>
2023-01-13 20:54:28 +0100

    added main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Auth from './pages/Auth';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,9 +17,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Auth />} />
+        </Routes>
+      </main>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
