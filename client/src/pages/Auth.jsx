065a728598ae620d4138c44b8f122b2deee2dc52
ivica <blizujemars@gmail.com>
2023-01-13 20:55:53 +0100

    Auth page

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthForm from '../components/Auth/AuthForm';
+
+const Auth = () => {
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  );
+};
+
+export default Auth;
