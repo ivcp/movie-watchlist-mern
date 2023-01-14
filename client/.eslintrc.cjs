9f58e883be2fd8ba23cc526f4d911992f3b233d0
ivica <blizujemars@gmail.com>
2023-01-14 18:31:38 +0100

    line error

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -20,7 +20,6 @@ module.exports = {
   },
   plugins: ['react', 'jest', 'cypress'],
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
