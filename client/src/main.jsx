4c00d87f0e2e478f3055e5c276d122f53bd3f29c
ivica <blizujemars@gmail.com>
2023-01-15 18:07:31 +0100

    wrap with UserContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { UserContextProvider } from './store/user-context';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </BrowserRouter>
 );
