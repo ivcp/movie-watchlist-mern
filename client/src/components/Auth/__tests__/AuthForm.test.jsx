57f5a6d8f67470bf3f76f447ae80d028f4945040
ivica <blizujemars@gmail.com>
2023-01-15 13:47:34 +0100

    add providers

--- client/src/components/Auth/__tests__/AuthForm.test.jsx
+++ client/src/components/Auth/__tests__/AuthForm.test.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-//import { vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import AuthForm from '../AuthForm';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 describe('Auth component', () => {
+  beforeEach(() => {
+    const queryClient = new QueryClient();
+    render(
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_PUBLIC_GOOGLE_API_TOKEN}
+      >
+        <QueryClientProvider client={queryClient}>
+          <AuthForm />
+        </QueryClientProvider>
+      </GoogleOAuthProvider>
+    );
+  });
+
   it('renders login form initially', async () => {
-    render(<AuthForm />);
     screen.getByRole('heading', /log in/i);
     screen.getByPlaceholderText(/email/i);
     screen.getByPlaceholderText(/password/i);
@@ -17,7 +30,6 @@ describe('Auth component', () => {
   });
 
   it('changes form when clicked to show register', async () => {
-    render(<AuthForm />);
     screen.getByRole('heading', { name: /log in/i });
     const user = userEvent.setup();
     const changeFormBtn = screen.getByRole('button', {
@@ -32,7 +44,6 @@ describe('Auth component', () => {
     ).not.toBeInTheDocument();
   });
   it('changes password visibility', async () => {
-    render(<AuthForm />);
     const passwordInput = screen.getByPlaceholderText(/password/i);
     expect(passwordInput).toHaveAttribute('type', 'password');
     const user = userEvent.setup();
@@ -41,15 +52,5 @@ describe('Auth component', () => {
     });
     await user.click(showPasswordBtn);
     expect(passwordInput).toHaveAttribute('type', 'text');
-  });
-
-  it.todo('request called with corret values', async () => {
-    //render(<AuthForm />);
-    // const user = userEvent.setup();
-    // const emailInput = screen.getByPlaceholderText(/email/i);
-    // const passwordInput = screen.getByPlaceholderText(/password/i);
-    //const submitBtn = screen.getByRole('button', { name: /log in/i });
-    // await user.type(emailInput, 'test@email.com');
-    // await user.type(passwordInput, '123456');
   });
 });
