44781de9a50d32ecd2f96bd649c3b746caaf6647
ivica <blizujemars@gmail.com>
2023-01-16 21:42:24 +0100

    remove div

---
+++ client/src/pages/Watchlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieList from '../components/MovieList/MovieList';
+
+const Watchlist = () => {
+  return (
+    <>
+      <MovieList />
+    </>
+  );
+};
+
+export default Watchlist;
