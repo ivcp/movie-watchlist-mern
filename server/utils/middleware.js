1f4ca1482a4f098fc4cb6c1d15d8ed119b7a26bb
ivica <blizujemars@gmail.com>
2023-01-17 18:33:01 +0100

    add casterror

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -17,6 +17,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: error.message });
   }
+  if (error.name === 'CastError') {
+    return response.status(401).json({ error: error.message });
+  }
 
   next(error);
 };
