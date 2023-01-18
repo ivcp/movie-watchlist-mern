5250f640cf7147fa6008f52c8a7c559d42e07741
ivica <blizujemars@gmail.com>
2023-01-18 10:46:17 +0100

    rtl config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,7 +10,9 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:cypress/recommended',
     'prettier',
+    'plugin:testing-library/react',
   ],
+  plugins: ['testing-library'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -30,6 +32,11 @@ module.exports = {
     'no-console': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off',
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
+    'testing-library/prefer-screen-queries': 'warn',
   },
   settings: {
     react: {
