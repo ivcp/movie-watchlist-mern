6137c5dc600745d194332338a133566acecd6091
ivica <blizujemars@gmail.com>
2023-01-22 12:10:43 +0100

    refactor movie sorting

---
+++ client/src/hooks/useMovieList.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext } from 'react';
+import UserContext from '../store/user-context';
+import { useQuery } from 'react-query';
+import movieService from '../services/movies';
+import MovieOnList from '../components/MovieList/MovieOnList';
+
+const useMovieList = () => {
+  const { user } = useContext(UserContext);
+  const [sort, setSort] = useState('all');
+  const { data, isLoading, isError, isSuccess, error } = useQuery(
+    ['user', user?.id],
+    movieService.getUserMovieList.bind(null, user?.id),
+    {
+      enabled: !!user,
+    }
+  );
+
+  const movieOnList = movie => <MovieOnList key={movie.id} movie={movie} />;
+
+  const sortedMovies = () => {
+    if (sort === 'all') {
+      return data.map(movieOnList);
+    }
+    if (sort === 'watched') {
+      return data.filter(movie => movie.watched).map(movieOnList);
+    }
+    if (sort === 'unwatched') {
+      return data.filter(movie => !movie.watched).map(movieOnList);
+    }
+  };
+
+  return {
+    user,
+    sort,
+    setSort,
+    sortedMovies,
+    data,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+  };
+};
+
+export default useMovieList;
