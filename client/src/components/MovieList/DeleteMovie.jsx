f7765382476ec97acf570914b1aa21cabafc6610
ivica <blizujemars@gmail.com>
2023-01-23 21:01:58 +0100

    add delete movie component

---
+++ client/src/components/MovieList/DeleteMovie.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import ModalContext from '../../store/modal-context';
+import PropTypes from 'prop-types';
+
+const DeleteMovie = props => {
+  const { triggerPrompt } = useContext(ModalContext);
+
+  return (
+    <button onClick={() => triggerPrompt(props.movie)}>{props.text}</button>
+  );
+};
+
+export default DeleteMovie;
+
+DeleteMovie.defaultProps = {
+  text: 'delete movie',
+};
+
+DeleteMovie.propTypes = {
+  movie: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired,
+};
