98cdcb70dc4e0e4cbfebe73a67fb537f5ea0ffe1
ivica <blizujemars@gmail.com>
2023-01-23 21:04:15 +0100

    conditionally render header

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from '../components/Navigation/Navigation';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
+  const location = useLocation();
   return (
     <>
-      <header>
-        <div>Watchlist</div>
-        <Navigation />
-      </header>
+      {/* TODO:add check if !isDesktop */}
+      {location.pathname.includes('/movie/') ? null : (
+        <header>
+          <div>Watchlist</div>
+          <Navigation />
+        </header>
+      )}
       <main>
         <Outlet />
       </main>
