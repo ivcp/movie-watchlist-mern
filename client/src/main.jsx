682c6184bf006c280501476e35281f996bc2f24b
ivica <blizujemars@gmail.com>
2023-01-30 16:39:53 +0100

    add global styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/800.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
