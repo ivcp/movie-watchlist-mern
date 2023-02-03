be355e6cffa588e6b545139273fc5b3acd60aace
ivica <blizujemars@gmail.com>
2023-02-03 21:05:42 +0100

    add emojis

--- client/src/helpers/genres.js
+++ client/src/helpers/genres.js
@@ -2,82 +2,103 @@ const genres = [
   {
     id: 'all',
     name: 'All',
-  },
-  {
-    id: 28,
-    name: 'Action',
-  },
-  {
-    id: 12,
-    name: 'Adventure',
-  },
-  {
-    id: 16,
-    name: 'Animation',
+    emoji: '🍿',
   },
   {
     id: 35,
     name: 'Comedy',
-  },
-  {
-    id: 80,
-    name: 'Crime',
-  },
-  {
-    id: 99,
-    name: 'Documentary',
-  },
-  {
-    id: 18,
-    name: 'Drama',
-  },
-  {
-    id: 10751,
-    name: 'Family',
-  },
-  {
-    id: 14,
-    name: 'Fantasy',
-  },
-  {
-    id: 36,
-    name: 'History',
-  },
-  {
-    id: 27,
-    name: 'Horror',
-  },
-  {
-    id: 10402,
-    name: 'Music',
-  },
-  {
-    id: 9648,
-    name: 'Mystery',
+    emoji: '😆',
   },
   {
     id: 10749,
     name: 'Romance',
+    emoji: '😍',
+  },
+  {
+    id: 27,
+    name: 'Horror',
+    emoji: '😨',
+  },
+  {
+    id: 28,
+    name: 'Action',
+    emoji: '🏃',
+  },
+  {
+    id: 12,
+    name: 'Adventure',
+    emoji: '🧗',
+  },
+  {
+    id: 16,
+    name: 'Animation',
+    emoji: '👹',
+  },
+  {
+    id: 80,
+    name: 'Crime',
+    emoji: '💣',
+  },
+  {
+    id: 99,
+    name: 'Documentary',
+    emoji: '📽️',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+    emoji: '😮',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+    emoji: '👪',
+  },
+  {
+    id: 14,
+    name: 'Fantasy',
+    emoji: '🧙',
+  },
+  {
+    id: 36,
+    name: 'History',
+    emoji: '🗺️',
+  },
+
+  {
+    id: 10402,
+    name: 'Music',
+    emoji: '🎵',
+  },
+  {
+    id: 9648,
+    name: 'Mystery',
+    emoji: '🤔',
   },
   {
     id: 878,
     name: 'Science Fiction',
+    emoji: '👽',
   },
   {
     id: 10770,
     name: 'TV Movie',
+    emoji: '📺',
   },
   {
     id: 53,
     name: 'Thriller',
+    emoji: '😲',
   },
   {
     id: 10752,
     name: 'War',
+    emoji: '🪖',
   },
   {
     id: 37,
     name: 'Western',
+    emoji: '🤠',
   },
 ];
 
