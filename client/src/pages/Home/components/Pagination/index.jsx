a2b6c971640cb2b589d06dab26957d3273d91b58
ivica <blizujemars@gmail.com>
2023-02-05 20:56:17 +0100

    add pagination styles, scroll to top

--- client/src/pages/Home/components/Pagination/index.jsx
+++ client/src/pages/Home/components/Pagination/index.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles.module.css';
+import genreStyles from '../GenreFilter/styles.module.css';
+import { TbChevronRight } from 'react-icons/tb';
+import { TbChevronLeft } from 'react-icons/tb';
+import useMediaQuery from '../../../../hooks/useMediaQuery';
 
 const Pagination = ({ page, moviesPage, totalPages, setPage }) => {
+  const isDesktop = useMediaQuery('(min-width: 37.5em)');
   return (
-    <div>
+    <div className={styles.pagination}>
       <button
-        onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+        className={isDesktop ? genreStyles.navButton : styles.mobileButton}
+        onClick={() => {
+          setPage(prev => Math.max(prev - 1, 1));
+          window.scrollTo(0, 0);
+        }}
         disabled={page === 1}
       >
+        <TbChevronLeft size={15} />
         <span>previous page</span>
       </button>
-      {page}
       <button
-        onClick={() =>
+        className={isDesktop ? genreStyles.navButton : styles.mobileButton}
+        onClick={() => {
           setPage(prev =>
             moviesPage === 500 || moviesPage === totalPages ? prev : prev + 1
-          )
-        }
+          );
+          window.scrollTo(0, 0);
+        }}
         disabled={moviesPage === 500}
       >
+        <TbChevronRight size={15} />
         <span>next page</span>
       </button>
     </div>
