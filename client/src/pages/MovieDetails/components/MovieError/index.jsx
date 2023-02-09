92c65001e90cdd48a1d4f24a03493f06972de4d8
ivica <blizujemars@gmail.com>
2023-02-09 18:33:32 +0100

    style error

--- client/src/pages/MovieDetails/components/MovieError/index.jsx
+++ client/src/pages/MovieDetails/components/MovieError/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
-
+import styles from './style.module.css';
 const MovieError = () => {
   const error = useRouteError();
   return (
-    <div>
+    <div className={styles.error}>
       <h4>{error.message}</h4>
     </div>
   );
