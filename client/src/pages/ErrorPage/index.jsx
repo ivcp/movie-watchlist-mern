3b2cb62fd43510863e7a0ac1c98e8e17cf60de6b
ivica <blizujemars@gmail.com>
2023-02-09 20:42:00 +0100

    error page

---
+++ client/src/pages/ErrorPage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const ErrorPage = () => {
+  return <div className={styles.error}>Page not found</div>;
+};
+
+export default ErrorPage;
