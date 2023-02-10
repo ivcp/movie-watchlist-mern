c036bf193118bceefee29b01e9331b2cba05ea8c
ivica <blizujemars@gmail.com>
2023-02-10 17:19:49 +0100

    fix spelling err

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const { MONGODB_URI } = require('./utils/config');
 const cors = require('cors');
-const tbdmRouter = require('./controllers/tmdb');
+const tmdbRouter = require('./controllers/tmdb');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const moviesRouter = require('./controllers/movies');
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/tmdb', tbdmRouter);
+app.use('/api/tmdb', tmdbRouter);
 
 // if (process.env.NODE_ENV === 'test') {
 //   const testingRouter = require('./controllers/testing');
