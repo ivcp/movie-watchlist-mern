c8652157d6446c80209ccfb6664293584cdf52f2
ivica <blizujemars@gmail.com>
2023-02-11 15:45:13 +0100

    refactor tests

--- client/cypress/e2e/watchlist.cy.js
+++ client/cypress/e2e/watchlist.cy.js
@@ -14,71 +14,68 @@ describe('Watchlist app', function () {
     cy.contains('home');
     cy.contains('log in');
   });
-  it('can register user', function () {
-    cy.contains('log in').click();
-    //prettier-ignore
-    cy.contains('don\'t have an account? register').click();
+  describe('auth', function () {
+    beforeEach(function () {
+      cy.contains('log in').click();
+      //prettier-ignore
+      cy.contains('don\'t have an account? register').click();
+      cy.get('input[placeholder="first name*"]').as('firstName');
+      cy.get('input[placeholder="last name"]').as('lastName');
+      cy.get('input[placeholder="email*"]').as('email');
+      cy.get('input[placeholder="password*"]').as('password');
+    });
 
-    cy.get('input[placeholder="first name*"]').type('tester');
-    cy.get('input[placeholder="last name"]').type('testing');
-    cy.get('input[placeholder="email*"]').type('test@email.com');
-    cy.get('input[placeholder="password*"]').type('123456');
-    cy.get('input[placeholder="password*"]').should(
-      'have.attr',
-      'type',
-      'password'
-    );
-    cy.contains('show password').click();
-    cy.get('input[placeholder="password*"]').should(
-      'have.attr',
-      'type',
-      'text'
-    );
-    cy.contains('register and log in').click();
-    cy.contains('tester testing registered successfully');
-    cy.contains('my movies 0');
-  });
-  it('does not register user if email taken, password too short, name too long', function () {
-    cy.contains('log in').click();
-    //prettier-ignore
-    cy.contains('don\'t have an account? register').click();
-    //if existing email
-    cy.get('input[placeholder="first name*"]').type('user');
-    cy.get('input[placeholder="last name"]').type('testing');
-    cy.get('input[placeholder="email*"]').type('user@email.com');
-    cy.get('input[placeholder="password*"]').type('123456');
-    cy.contains('register and log in').click();
-    cy.contains('user@email.com already taken');
-    //if short password
-    cy.get('input[placeholder="email*"]').clear().type('new@email.com');
-    cy.get('input[placeholder="password*"]').clear().type('123');
-    cy.contains('register and log in').click();
-    cy.contains('password must be at least 6 characters long');
-    //if long name
-    cy.get('input[placeholder="password*"]').type('123456');
-    cy.get('input[placeholder="first name*"]').clear().type('reallylongname');
-    cy.contains('register and log in').click();
-    cy.contains('name cannot be more than 12 characters long');
-  });
+    it('can register user', function () {
+      cy.get('@firstName').type('tester');
+      cy.get('@lastName').type('testing');
+      cy.get('@email').type('test@email.com');
+      cy.get('@password').type('123456');
+      cy.get('@password').should('have.attr', 'type', 'password');
+      cy.contains('show password').click();
+      cy.get('@password').should('have.attr', 'type', 'text');
+      cy.contains('register and log in').click();
+      cy.contains('tester testing registered successfully');
+      cy.contains('my movies 0');
+    });
+    it('does not register user if email taken, password too short, name too long', function () {
+      //if existing email
+      cy.get('@firstName').type('user');
+      cy.get('@lastName').type('testing');
+      cy.get('@email').type('user@email.com');
+      cy.get('@password').type('123456');
+      cy.contains('register and log in').click();
+      cy.contains('user@email.com already taken');
+      //if short password
+      cy.get('@email').clear().type('new@email.com');
+      cy.get('@password').clear().type('123');
+      cy.contains('register and log in').click();
+      cy.contains('password must be at least 6 characters long');
+      //if long name
+      cy.get('@password').type('123456');
+      cy.get('@firstName').clear().type('reallylongname');
+      cy.contains('register and log in').click();
+      cy.contains('name cannot be more than 12 characters long');
+    });
 
-  it('can log in user', function () {
-    cy.contains('log in').click();
-    cy.get('input[placeholder="email*"]').type('user@email.com');
-    cy.get('input[placeholder="password*"]').type('123456');
-    cy.get('#submit-credentials').click();
-    cy.contains('user existing logged in successfully');
-  });
-  it.only('fails to log in if wrong credentials', function () {
-    cy.contains('log in').click();
-    // wrong email
-    cy.get('input[placeholder="email*"]').type('new@email.com');
-    cy.get('input[placeholder="password*"]').type('123456');
-    cy.get('#submit-credentials').click();
-    cy.contains('user with email new@email.com does not exist');
-    // wrong password
-    cy.get('input[placeholder="email*"]').clear().type('user@email.com');
-    cy.get('input[placeholder="password*"]').clear().type('1234567');
-    cy.get('#submit-credentials').click();
-    cy.contains('invalid password');
+    it('can log in user', function () {
+      cy.contains('already have an account? log in').click();
+      cy.get('@email').type('user@email.com');
+      cy.get('@password').type('123456');
+      cy.get('#submit-credentials').click();
+      cy.contains('user existing logged in successfully');
+    });
+    it('fails to log in if wrong credentials', function () {
+      cy.contains('already have an account? log in').click();
+      // wrong email
+      cy.get('@email').type('new@email.com');
+      cy.get('@password').type('123456');
+      cy.get('#submit-credentials').click();
+      cy.contains('user with email new@email.com does not exist');
+      // wrong password
+      cy.get('@email').clear().type('user@email.com');
+      cy.get('@password').clear().type('1234567');
+      cy.get('#submit-credentials').click();
+      cy.contains('invalid password');
+    });
   });
 });
