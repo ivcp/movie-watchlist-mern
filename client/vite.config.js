47f205d0185114eaa6c7b9e854776939f74ce057
ivica <blizujemars@gmail.com>
2023-02-13 13:19:58 +0100

    configure proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.js',
   },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
-    },
-  },
+};
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isDev = mode === 'development';
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': isDev
+            ? 'http://localhost:3001'
+            : 'https://watchlist-6wzf.onrender.com',
+        },
+      },
+    };
+  } else {
+    return defaultConfig;
+  }
 });
